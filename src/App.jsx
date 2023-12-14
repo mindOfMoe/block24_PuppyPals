fefe554fd6467121b6deea6dcc1abf0c51d5dc4b
Moises Rivera <morivera2015@gmail.com>
2023-12-14 01:56:19 -0500

    created div to display puppy name

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-//import { useState } from "react";
+import { useState } from "react";
+import { puppyList } from "./data.js";
 
 function App() {
+  //console.log(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
     <>
-      <div></div>
+      <div>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
     </>
   );
 }
