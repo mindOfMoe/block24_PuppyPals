561d6deb33eca0203349fd2074aff6e05d4e53ae
Moises Rivera <morivera2015@gmail.com>
2023-12-17 02:53:21 -0500

    comp'd selecting a puppy feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
+//to utilize the useState hook feature of react
 import { useState } from "react";
+// import the data from the data.js file
 import { puppyList } from "./data.js";
 
 function App() {
-  //console.log(puppyList);
+  //
   const [puppies, setPuppies] = useState(puppyList);
+  //
+  const [featPupId, setFeatPupId] = useState(null);
+
   console.log(puppies);
+  const featuredPup = puppies.find((pup) => {
+    return pup.id === featPupId;
+  });
+  console.log(featuredPup);
   return (
     <>
       <div>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })}
       </div>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>{featuredPup.age}</li>
+            <li>{featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
